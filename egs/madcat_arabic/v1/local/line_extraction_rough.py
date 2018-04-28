6bb3db3c6b9383623d7d6ed0452c7c925424cfc1
aarora8 <aarora8@jhu.edu>
2018-04-28 19:36:45 -0400

    adding documentation

--- egs/madcat_arabic/v1/local/line_extraction_rough.py
+++ egs/madcat_arabic/v1/local/line_extraction_rough.py
@@ -1,53 +1,25 @@
-#!/usr/bin/env python3
-
-# Copyright   2018 Ashish Arora
-# Apache 2.0
-# minimum bounding box part in this script is originally from
-#https://github.com/BebeSparkelSparkel/MinimumBoundingBox
-
-""" This module will be used for creating text localization mask on page image.
- Given the word segmentation (bounding box around a word) for every word, it will
- extract line segmentation. To extract line segmentation, it will take word bounding
- boxes of a line as input, will create a minimum area bounding box that will contain
- all corner points of word bounding boxes. The obtained bounding box (will not necessarily
- be vertically or horizontally aligned). To obtain the pixel mask, page image is
- rotated to make the bounding box horizontal. In the horizontal bounding box
- pixel locations in the box are reversed mapped to unrotated image
-"""
-
-import sys
 import argparse
 import os
 import xml.dom.minidom as minidom
 import numpy as np
-from math import atan2, cos, sin, pi, degrees, sqrt
-from collections import namedtuple
+
+import matplotlib
+matplotlib.use('TkAgg')
+
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+from matplotlib.patches import Arrow, Circle
 
 from scipy.spatial import ConvexHull
+from math import atan2, cos, sin, pi, degrees, sqrt
+from collections import namedtuple
+from skimage.io import imshow, show, imread, imsave
+from skimage.transform import rotate
+from skimage import img_as_uint, img_as_float
+from skimage import io
 from PIL import Image
 from scipy.misc import toimage
-import logging
 
-sys.path.insert(0, 'steps')
-logger = logging.getLogger('libs')
-logger.setLevel(logging.INFO)
-handler = logging.StreamHandler()
-handler.setLevel(logging.INFO)
-formatter = logging.Formatter("%(asctime)s [%(pathname)s:%(lineno)s - "
-                              "%(funcName)s - %(levelname)s ] %(message)s")
-handler.setFormatter(formatter)
-logger.addHandler(handler)
-
-"""
-bounding_box is a named tuple which contains:
-             area (float): area of the rectangle
-             length_parallel (float): length of the side that is parallel to unit_vector
-             length_orthogonal (float): length of the side that is orthogonal to unit_vector
-             rectangle_center(int, int): coordinates of the rectangle center
-             unit_vector (float, float): direction of the length_parallel side.
-             unit_vector_angle (float): angle of the unit vector to be in radians.
-             corner_points [(float, float)]: set that contains the corners of the rectangle
-"""
 bounding_box_tuple = namedtuple('bounding_box_tuple', 'area '
                                         'length_parallel '
                                         'length_orthogonal '
@@ -176,19 +148,14 @@ def get_smaller_angle(bounding_box):
         return ortho_vector_angle_updated
 
 
-def rotated_points(bounding_box, center, if_opposite_direction=False):
+def rotated_points(bounding_box, center):
     p1, p2, p3, p4 = bounding_box.corner_points
     x1, y1 = p1
     x2, y2 = p2
     x3, y3 = p3
     x4, y4 = p4
     center_x, center_y = center
-
-    if if_opposite_direction:
-        rotation_angle_in_rad = get_smaller_angle(bounding_box)
-    else:
-        rotation_angle_in_rad = -get_smaller_angle(bounding_box)
-
+    rotation_angle_in_rad = -get_smaller_angle(bounding_box)
     x_dash_1 = (x1 - center_x) * cos(rotation_angle_in_rad) - (y1 - center_y) * sin(rotation_angle_in_rad) + center_x
     x_dash_2 = (x2 - center_x) * cos(rotation_angle_in_rad) - (y2 - center_y) * sin(rotation_angle_in_rad) + center_x
     x_dash_3 = (x3 - center_x) * cos(rotation_angle_in_rad) - (y3 - center_y) * sin(rotation_angle_in_rad) + center_x
@@ -198,7 +165,6 @@ def rotated_points(bounding_box, center, if_opposite_direction=False):
     y_dash_2 = (y2 - center_y) * cos(rotation_angle_in_rad) + (x2 - center_x) * sin(rotation_angle_in_rad) + center_y
     y_dash_3 = (y3 - center_y) * cos(rotation_angle_in_rad) + (x3 - center_x) * sin(rotation_angle_in_rad) + center_y
     y_dash_4 = (y4 - center_y) * cos(rotation_angle_in_rad) + (x4 - center_x) * sin(rotation_angle_in_rad) + center_y
-
     return x_dash_1, y_dash_1, x_dash_2, y_dash_2, x_dash_3, y_dash_3, x_dash_4, y_dash_4
 
 
@@ -237,13 +203,15 @@ def set_line_image_data(image, line_id, image_file_name):
 
 
 def get_line_images_from_page_image(image_file_name, madcat_file_path):
+    im_wo_pad = Image.open(image_file_name)
+    im = pad_image(im_wo_pad)
     doc = minidom.parse(madcat_file_path)
     zone = doc.getElementsByTagName('zone')
     for node in zone:
         id = node.getAttribute('id')
         print(id)
-        if id != 'z1':
-            continue
+        # if id != 'z1':
+        #     continue
         token_image = node.getElementsByTagName('token-image')
         minimum_bounding_box_input = []
         for token_node in token_image:
@@ -256,7 +224,6 @@ def get_line_images_from_page_image(image_file_name, madcat_file_path):
         bounding_box = minimum_bounding_box(updated_mbb_input)
 
         p1, p2, p3, p4 = bounding_box.corner_points
-        print(p1, p2, p3, p4)
         x1, y1 = p1
         x2, y2 = p2
         x3, y3 = p3
@@ -265,30 +232,39 @@ def get_line_images_from_page_image(image_file_name, madcat_file_path):
         min_y = int(min(y1, y2, y3, y4))
         max_x = int(max(x1, x2, x3, x4))
         max_y = int(max(y1, y2, y3, y4))
-        center_x = (max_x - min_x) / 2
-        center_y = (max_y - min_y) / 2
+        box = (min_x, min_y, max_x, max_y)
+        region_initial = im.crop(box)
+        # ax.imshow(region_initial)
+        # plt.show()
+        # region_initial = im[min_y:max_y, min_x:max_x]
 
+        # #calculate new crop points
         rot_points = []
         p1_new = (x1 - min_x, y1 - min_y)
         p2_new = (x2 - min_x, y2 - min_y)
         p3_new = (x3 - min_x, y3 - min_y)
         p4_new = (x4 - min_x, y4 - min_y)
-        print(p1_new, p2_new, p3_new, p4_new)
-
         rot_points.append(p1_new)
         rot_points.append(p2_new)
         rot_points.append(p3_new)
         rot_points.append(p4_new)
         cropped_bounding_box = bounding_box_tuple(bounding_box.area,
-                                                  bounding_box.length_parallel,
-                                                  bounding_box.length_orthogonal,
-                                                  bounding_box.length_orthogonal,
-                                                  bounding_box.unit_vector,
-                                                  bounding_box.unit_vector_angle,
-                                                  set(rot_points)
-                                                  )
+            bounding_box.length_parallel,
+            bounding_box.length_orthogonal,
+            bounding_box.length_orthogonal,
+            bounding_box.unit_vector,
+            bounding_box.unit_vector_angle,
+            set(rot_points)
+        )
+
+        rotation_angle_in_rad = get_smaller_angle(cropped_bounding_box)
+        img2 = region_initial.rotate(degrees(rotation_angle_in_rad), resample=Image.BICUBIC)
+        # ax.imshow(img2)
+        # plt.show()
+        #img2 = rotate(region_initial, degrees(rotation_angle_in_rad), order=3)
         x_dash_1, y_dash_1, x_dash_2, y_dash_2, x_dash_3, y_dash_3, x_dash_4, y_dash_4 = rotated_points(
-            cropped_bounding_box, (center_x , center_y))
+            cropped_bounding_box, get_center(region_initial))
+
         print(x_dash_1, y_dash_1, x_dash_2, y_dash_2, x_dash_3, y_dash_3, x_dash_4, y_dash_4)
 
         min_x = int(min(x_dash_1, x_dash_2, x_dash_3, x_dash_4))
@@ -296,38 +272,18 @@ def get_line_images_from_page_image(image_file_name, madcat_file_path):
         max_x = int(max(x_dash_1, x_dash_2, x_dash_3, x_dash_4))
         max_y = int(max(y_dash_1, y_dash_2, y_dash_3, y_dash_4))
         box = (min_x, min_y, max_x, max_y)
-        
-
-        rot_points = []
-        p1_new = (x_dash_1, y_dash_1)
-        p2_new = (x_dash_2, y_dash_2)
-        p3_new = (x_dash_3, y_dash_3)
-        p4_new = (x_dash_4, y_dash_4)
-        rot_points.append(p1_new)
-        rot_points.append(p2_new)
-        rot_points.append(p3_new)
-        rot_points.append(p4_new)
-        cropped_bounding_box = bounding_box_tuple(bounding_box.area,
-                                                  bounding_box.length_parallel,
-                                                  bounding_box.length_orthogonal,
-                                                  bounding_box.length_orthogonal,
-                                                  bounding_box.unit_vector,
-                                                  bounding_box.unit_vector_angle,
-                                                  set(rot_points)
-                                                  )
-        x_dash_1_old, y_dash_1_old, x_dash_2_old, y_dash_2_old, x_dash_3_old, y_dash_3_old, x_dash_4_old, y_dash_4_old = rotated_points(
-            cropped_bounding_box, (center_x, center_y), True)
-        print(x_dash_1_old, y_dash_1_old, x_dash_2_old, y_dash_2_old, x_dash_3_old, y_dash_3_old, x_dash_4_old, y_dash_4_old)
-
-        p1_new = (x1 - min_x, y1 - min_y)
-        p2_new = (x2 - min_x, y2 - min_y)
-        p3_new = (x3 - min_x, y3 - min_y)
-        p4_new = (x4 - min_x, y4 - min_y)
-
-
+        region_final = img2.crop(box)
+        ax.imshow(region_final)
+        plt.show()
+        #region_final = img2[min_y:max_y, min_x:max_x]
+        set_line_image_data(region_final, id, image_file_name)
+        # imshow(region_final)
+        # show()
+        # input("Press the <ENTER> key to continue...")
 
 
 ### main ###
+fig,ax = plt.subplots(1)
 line_images_path = '/Users/ashisharora/madcat_ar'
 data_path = '/Users/ashisharora/madcat_ar'
 for file in os.listdir(os.path.join(data_path, 'images')):
