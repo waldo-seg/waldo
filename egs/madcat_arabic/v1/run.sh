5d76585bc03766bb3a01dba5efde59a3ec656ae3
aarora8 <aarora8@jhu.edu>
2018-04-28 20:00:06 -0400

    adding bash files

--- egs/madcat_arabic/v1/run.sh
+++ egs/madcat_arabic/v1/run.sh
@@ -4,7 +4,13 @@
 set -e
 stage=0
 
-. ./scripts/parse_options.sh  # e.g. this parses the above options
-                            # if supplied.
 
-local/get_bounding_box_pixels.py
+. ./cmd.sh ## You'll want to change cmd.sh to something that will work on your system.
+           ## This relates to the queue.
+. ./path.sh
+. ./scripts/parse_options.sh  # e.g. this parses the above options
+                              # if supplied.
+
+if [ $stage -le 0 ]; then
+  local/get_bounding_box_pixels.py
+fi
