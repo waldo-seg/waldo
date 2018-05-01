d0807766a39242b9b66707920c97c4e1d4a07879
Ashish Arora <aarora8@jhu.edu>
2018-04-30 20:01:56 -0400

    Add the data preparation part for the MADCAT Arabic recipe
    * adding parse options
    
    * adding readme
    
    * adding code for line segmentation from xml and getting pixel mask
    
    * adding documentation
    
    * adding documentation
    
    * adding documentation
    
    * adding run.sh
    
    * adding bash files
    
    * updating readme
    
    * adding extract lines to modify it to extract pixels
    
    * updating run.sh
    
    * adding check tools
    
    * adding parse options
    
    * adding parse options
    
    * updating pixel extraction code
    
    * to add loop for pixels inside rect
    
    * add loop for pixels inside rect
    
    * add loop for pixels inside rect
    
    * add loop for pixels inside rect
    
    * adding changes from the review
    
    * updating run.sh, removing extra file, adding documentation, adding split_scp file
    
    * bug fix
    
    * python file rename
    
    * updating space
    
    * adding changes from the review

---
+++ egs/madcat_arabic/v1/run.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+
+set -e
+stage=1
+nj=70
+download_dir1=/export/corpora/LDC/LDC2012T15/data
+download_dir2=/export/corpora/LDC/LDC2013T09/data
+download_dir3=/export/corpora/LDC/LDC2013T15/data
+
+. ./cmd.sh ## You'll want to change cmd.sh to something that will work on your system.
+           ## This relates to the queue.
+. ./path.sh
+. ./scripts/parse_options.sh  # e.g. this parses the above options
+                              # if supplied.
+./local/check_tools.sh
+
+mkdir -p data/{train,test,dev}/data
+mkdir -p data/local/{train,test,dev}
+
+if [ $stage -le 0 ]; then
+  for dataset in test dev train; do
+    echo "$0: Extracting line images from page image for dataset:  $dataset. "
+    echo "Date: $(date)."
+    dataset_file=/home/kduh/proj/scale2018/data/madcat_datasplit/ar-en/madcat.$dataset.raw.lineid
+    local/extract_lines.sh --nj $nj --cmd $cmd --dataset_file $dataset_file \
+                           --download_dir1 $download_dir1 --download_dir2 $download_dir2 \
+                           --download_dir3 $download_dir3 data/local/$dataset
+  done
+fi
+
+mkdir -p data/{train,test,dev}/masks
+if [ $stage -le 1 ]; then
+  for dataset in test dev train; do
+    echo "$0: Extracting mask from page image for dataset:  $dataset. "
+    echo "Date: $(date)."
+    dataset_file=/home/kduh/proj/scale2018/data/madcat_datasplit/ar-en/madcat.$dataset.raw.lineid
+    local/extract_masks.py $download_dir1 $download_dir2 $download_dir3 $dataset_file data/$dataset/masks
+  done
+fi
