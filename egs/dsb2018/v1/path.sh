2a7d8e6c9a3bccceb67ccccf95eb0b94b5945846
Daniel Povey <dpovey@gmail.com>
2018-05-06 20:38:21 -0400

    add scripts/ to path.sh

--- egs/dsb2018/v1/path.sh
+++ egs/dsb2018/v1/path.sh
@@ -1,5 +1,6 @@
 export PATH="${HOME}/miniconda3/bin:$PATH"
 export PYTHONPATH="${PYTHONPATH}:${HOME}/miniconda3"
 
+export PYTHONPATH="${PYTHONPATH}:scripts"
 export PATH=$PWD/../../../scripts/parallel/:$PATH
 export PATH=$PWD/../../../scripts:$PATH
