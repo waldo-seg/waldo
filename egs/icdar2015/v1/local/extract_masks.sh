e3a4f5eca4898acf4932cf286ac98f9d433811a8
desh2608 <r.desh26@gmail.com>
2018-05-07 13:59:31 +0530

    changed indentation to 4 pt spaces

--- egs/icdar2015/v1/local/extract_masks.sh
+++ egs/icdar2015/v1/local/extract_masks.sh
@@ -3,12 +3,12 @@
 
 while [ $# -gt 0 ]; do
 
-   if [[ $1 == *"--"* ]]; then
-        v="${1/--/}"
-        declare $v="$2"
-   fi
+     if [[ $1 == *"--"* ]]; then
+                v="${1/--/}"
+                declare $v="$2"
+     fi
 
-  shift
+    shift
 done
 
 nj=4
@@ -26,30 +26,30 @@ log_dir=$data/log
 mkdir -p $log_dir
 
 for n in $(seq $nj); do
-  mkdir -p $data/$n
+    mkdir -p $data/$n
 done
 
 for dir in images labels; do
-  ls $( eval "echo \$${dir}" ) > ${log_dir}/${dir}_fn.txt
-  
-  total_lines=$(wc -l <${log_dir}/${dir}_fn.txt)
-  ((lines_per_file = (total_lines + nj - 1) / nj))
-  split --lines=${lines_per_file} -a 1 --numeric-suffixes=1 ${log_dir}/${dir}_fn.txt ${log_dir}/${dir}_fn_
+    ls $( eval "echo \$${dir}" ) > ${log_dir}/${dir}_fn.txt
+    
+    total_lines=$(wc -l <${log_dir}/${dir}_fn.txt)
+    ((lines_per_file = (total_lines + nj - 1) / nj))
+    split --lines=${lines_per_file} -a 1 --numeric-suffixes=1 ${log_dir}/${dir}_fn.txt ${log_dir}/${dir}_fn_
 
-  rm ${log_dir}/${dir}_fn.txt
+    rm ${log_dir}/${dir}_fn.txt
 done
 
 $cmd JOB=1:$nj $log_dir/extract_masks.JOB.log \
-  local/get_masks_from_page_image.py $images $labels $data/JOB $log_dir/lines.JOB.scp \
-  --images_fn=$log_dir/images_fn_JOB --labels_fn=$log_dir/labels_fn_JOB \
-  || exit 1;
+    local/get_masks_from_page_image.py $images $labels $data/JOB $log_dir/lines.JOB.scp \
+    --images_fn=$log_dir/images_fn_JOB --labels_fn=$log_dir/labels_fn_JOB \
+    || exit 1;
 
 ## concatenate the .scp files together.
 for n in $(seq $nj); do
-  cat $data/$n/images.scp || exit 1;
+    cat $data/$n/images.scp || exit 1;
 done > $data/images.scp || exit 1
 
 for n in $(seq $nj); do
-  rm $log_dir/images_fn_$n
-  rm $log_dir/labels_fn_$n
+    rm $log_dir/images_fn_$n
+    rm $log_dir/labels_fn_$n
 done
