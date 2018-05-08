4820a7a72ae3a8e337b1ebb6c62d325723931b3c
Ashish Arora <aarora8@jhu.edu>
2018-05-08 17:38:49 -0400

    Fill out some validation and mask generation code.

--- scripts/waldo/data_manipulation.py
+++ scripts/waldo/data_manipulation.py
@@ -1,10 +1,12 @@
 # Copyright      2018  Johns Hopkins University (author: Daniel Povey)
+#                2018  Desh Raj
+#                2018  Ashish Arora
 
 # Apache 2.0
 
-
-""" TODO
-"""
+import numpy as np
+from PIL import Image, ImageDraw
+from data_types import *
 
 
 def convert_to_mask(x):
@@ -14,7 +16,26 @@ def convert_to_mask(x):
      """
     validate_image_with_objects(x)
 
-    # TODO ...
+    im = x['img']
+    object_id = 0
+    y = dict()
+    y['img'] = im
+    mask_img = Image.new('L', (im.size[0], im.size[1]), 0)
+    mask_img_arr = np.array(mask_img)
+    object_class = list()
+    object_class.append(object_id)
+    for object in x['objects']:
+        ordered_polygon_points = object['polygon']
+        object_id += 1
+        temp_img = Image.new('L', (im.size[0], im.size[1]), 0)
+        ImageDraw.Draw(temp_img).polygon(ordered_polygon_points, fill=object_id)
+        temp_img_arr = np.array(temp_img)
+        pixels = np.where(temp_img_arr == object_id, temp_img_arr, mask_img_arr)
+        array = np.array(pixels, dtype=np.uint8)
+        new_image = Image.fromarray(array)
+        mask_img_arr = np.array(new_image)
+        object_class.append(object_id)
+    y['mask'] = mask_img_arr
 
     validate_image_with_mask(y)
     return y
@@ -27,8 +48,20 @@ def convert_polygon_to_points(polygon):
     """
     validate_polygon(polygon)
 
-    # TODO ...
+    ordered_polygon_points = polygon['polygon']
+    x_list = [i[0] for i in ordered_polygon_points]
+    y_list = [i[1] for i in ordered_polygon_points]
+    max_x = max(x_list)
+    max_y = max(y_list)
+    mask_image = Image.new('L', (max_x, max_y), 0)
+    ImageDraw.Draw(mask_image).polygon(ordered_polygon_points, fill=1)
+    mask_img_arr = np.array(mask_image)
+    points_location = np.where(mask_img_arr == 1)
+    points_list = []
+    for x, y in zip(points_location[0], points_location[1]):
+        points_list.append((x, y))
 
+    return points_list
 
 
 def convert_to_combined_image(x, c):
@@ -42,7 +75,8 @@ def convert_to_combined_image(x, c):
     """
     validate_config(c)
     validate_image_with_mask(x)
-    # TODO.. set y.
+    y = dict()
+    # # TODO.. set y.
     validate_combined_image(y, c)
     return y
 
@@ -55,6 +89,7 @@ def pad_combined_image(x, c):
     x is not modified; the padded image is returned.
     """
     validate_combined_image(x, c)
+    y = dict()
     if c['padding'] == 0:
         return None
 
@@ -72,6 +107,7 @@ def randomly_crop_combined_image(x, c):
     It is an error if the width or height of image x were previously smaller
     than that. """
     validate_combined_image(x, c)
+    y = dict()
     # TODO
     validate_combined_image(y, c)
     return y
