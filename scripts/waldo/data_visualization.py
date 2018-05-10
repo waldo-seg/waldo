33d5dc32d32b879d86ec4ef42e2b6ece372eeb0b
adel <rahimi.adel@gmail.com>
2018-05-11 00:50:52 +0430

    fixed the function so that it won't access disk and also the input is now NP arrays

--- scripts/waldo/data_visualization.py
+++ scripts/waldo/data_visualization.py
@@ -6,33 +6,22 @@
 """ TODO
 """
 
+def visualize_mask(x):
+    # "image" is a dictionary containing masks and image and numpy arrays
+    mask = x['mask']
+    red, green, blue, alpha = mask.T  # unpacking blends
+    white_areas = (red > 0) & (blue > 0) & (green > 0)
+    black_areas = (red == 0) & (blue == 0) & (green == 0) & (alpha == 255)
+    # randomly coloring white areas. low is 10 as a threshold.
+    # dimensions are = Red, Blue, Green, Alpha
+    # Alpha is set 38 for a ~20% transparency
+    mask[..., :][white_areas.T] = (
+        np.random.randint(low=10, high=255), np.random.randint(low=10, high=255),
+        np.random.randint(low=10, high=255), 38)
+    # removing black areas
+    mask[..., :][black_areas.T] = 0
 
-def visualize_mask(image, masks):
-	# image is the backgroun image, and masks is the directory to masks folder
-    image = Image.open(image)
-    background = image.convert('RGBA')
-    mask_background = Image.new('RGBA', (256, 256))
-    for filename in masks:
-        mask = Image.open(os.getcwd() + '/img/masks/' + filename)
-        rgbimg = mask.convert("RGBA")
-        data = np.array(rgbimg)  # "data" is a height x width x 4 numpy arrays
-        red, green, blue, alpha = data.T  # unpacking blends
-
-        white_areas = (red > 0) & (blue > 0) & (green > 0)
-        black_areas = (red == 0) & (blue == 0) & (green == 0) & (alpha == 255)
-        # randomly coloring white areas. low is 10 as threshold.
-        # dimensions are = Red, Blue, Green, Alpha
-        # Alpha is set 127 for a 50% reduced transparency
-        data[..., :][white_areas.T] = (
-            np.random.randint(low=10, high=255), np.random.randint(low=10, high=255),
-            np.random.randint(low=10, high=255), 51)
-        # removing black areas
-        data[..., :][black_areas.T] = 0
-
-        mask_transformed = Image.fromarray(data, mode='RGBA')
-        mask_background = Image.alpha_composite(mask_background, mask_transformed)
-
-    result = Image.alpha_composite(background, mask_background).save('Image.png')
+    x['mask'] = mask
     return None
     """This function accepts an object x that should represent an image with a
        mask, and it modifies the image to superimpose the "mask" on it.  The
