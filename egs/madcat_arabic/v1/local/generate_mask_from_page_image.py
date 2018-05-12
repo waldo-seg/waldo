61e70f5ad324e462e6406b1aa26a49a5009e93fc
aarora8 <aarora8@jhu.edu>
2018-05-12 05:22:53 -0400

    minor fix

--- egs/madcat_arabic/v1/local/generate_mask_from_page_image.py
+++ egs/madcat_arabic/v1/local/generate_mask_from_page_image.py
@@ -42,15 +42,15 @@ parser = argparse.ArgumentParser(description="Creates line images from page imag
                                              " data/LDC2013T09 data/LDC2013T15 data/madcat.train.raw.lineid "
                                              " data/local/lines ",
                                 formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-parser.add_argument('--database_path1', type=str,
+parser.add_argument('database_path1', type=str,
                     help='Path to the downloaded madcat data directory 1')
-parser.add_argument('--database_path2', type=str,
+parser.add_argument('database_path2', type=str,
                     help='Path to the downloaded madcat data directory 2')
-parser.add_argument('--database_path3', type=str,
+parser.add_argument('database_path3', type=str,
                     help='Path to the downloaded madcat data directory 3')
-parser.add_argument('--data_splits', type=str,
+parser.add_argument('data_splits', type=str,
                     help='Path to file that contains the train/test/dev split information')
-parser.add_argument('--out_dir', type=str,
+parser.add_argument('out_dir', type=str,
                     help='directory location to write output files')
 parser.add_argument('--padding', type=int, default=400,
                     help='padding across horizontal/verticle direction')
@@ -207,15 +207,18 @@ def check_writing_condition(wc_dict, base_name):
 
 
 def main():
-    args.database_path1 = "/Users/ashisharora/google_Drive/madcat_arabic/LDC2012T15"
-    args.database_path2 = "/Users/ashisharora/google_Drive/madcat_arabic/LDC2013T09"
-    args.database_path3 = "/Users/ashisharora/google_Drive/madcat_arabic/LDC2013T15"
-    args.data_splits = "/Users/ashisharora/google_Drive/madcat_arabic/madcat.dev.raw.lineid"
-    args.out_dir = "/Users/ashisharora/google_Drive/madcat_arabic/masks"
+    writing_condition_folder_list = args.database_path1.split('/')
+    writing_condition_folder1 = ('/').join(writing_condition_folder_list[:5])
 
-    writing_conditions1 = os.path.join(args.database_path1, 'writing_conditions.tab')
-    writing_conditions2 = os.path.join(args.database_path2, 'writing_conditions.tab')
-    writing_conditions3 = os.path.join(args.database_path3, 'writing_conditions.tab')
+    writing_condition_folder_list = args.database_path2.split('/')
+    writing_condition_folder2 = ('/').join(writing_condition_folder_list[:5])
+
+    writing_condition_folder_list = args.database_path3.split('/')
+    writing_condition_folder3 = ('/').join(writing_condition_folder_list[:5])
+
+    writing_conditions1 = os.path.join(writing_condition_folder1, 'docs', 'writing_conditions.tab')
+    writing_conditions2 = os.path.join(writing_condition_folder2, 'docs', 'writing_conditions.tab')
+    writing_conditions3 = os.path.join(writing_condition_folder3, 'docs', 'writing_conditions.tab')
 
     wc_dict1 = parse_writing_conditions(writing_conditions1)
     wc_dict2 = parse_writing_conditions(writing_conditions2)
