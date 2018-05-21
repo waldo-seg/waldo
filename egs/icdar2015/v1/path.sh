17deaa4fa8cfd857952317d60382e1fc1bc697e0
huangruizhe <eraser567@163.com>
2018-05-21 10:46:52 -0400

    remove duplicates in path.sh for icdar2015

--- egs/icdar2015/v1/path.sh
+++ egs/icdar2015/v1/path.sh
@@ -3,3 +3,8 @@ export PATH=$PWD/../../../scripts/parallel/:$PATH
 export PATH=$PWD/../../../scripts/waldo:$PATH
 export PATH=$PWD/../../../scripts:$PATH
 export PYTHONPATH=${PYTHONPATH}:$PWD/../../../scripts
+
+
+# Here is a shell snippet that removes duplicates from $PATH. 
+# Ref: http://linuxg.net/oneliners-for-removing-the-duplicates-in-your-path/
+export PATH=`echo -n $PATH | awk -v RS=: -v ORS=: '{ if (!arr[$0]++) { print $0 } }'`
